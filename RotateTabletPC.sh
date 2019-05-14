ef9eb7de711592aa81ec944c9f79e36bb6c6a600
Fumito NISHIKAWA <fmt.floatlamp+git@gmail.com>
2019-05-14 21:44:17 +0900

    Test and some fix.

--- RotateTabletPC.sh
+++ RotateTabletPC.sh
@@ -1,15 +1,21 @@
-#とりあえずはシンプル切り替えられるように通常の画面をPCモード、縦画面をタブレットモードとして切り替える感じにする
-#対象のデバイスは　xinput list で確認
-#自分の場合は1１がマルチタッチ、18がペンらしかったのでそれを画面と一緒に回転
+#!/bin/bash
+
+#シンプルに通常の画面をPCモード、縦画面をタブレットモードとして切り替える
+
+#対象のデバイスのIDは　xinput list で確認
+touch_display_multi=11
+touch_display_stylus=18
+trackpad=13
 
 rotation=$(xrandr -q --verbose | grep eDP-1 | egrep -o "(normal|left)" | head -1)
 if [ "$rotation" = "normal" ] ; then
-    xrandr --output eDP-1 --rotate right
-    xinput set-prop 11 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
-    xinput set-prop 18 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
-
+    xrandr --output eDP-1 --rotate left
+    xinput set-prop $touch_display_multi 'Coordinate Transformation Matrix' 0 -1 1 1 0 0 0 0 1
+    xinput set-prop $touch_display_stylus 'Coordinate Transformation Matrix' 0 -1 1 1 0 0 0 0 1
+    xinput set-prop $trackpad 'Device Enabled' 0
 else
     xrandr --output eDP-1 --rotate normal
-    xinput set-prop 11 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
-    xinput set-prop 18 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
+    xinput set-prop $touch_display_multi 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
+    xinput set-prop $touch_display_stylus 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
+    xinput set-prop $trackpad 'Device Enabled' 1
 fi
