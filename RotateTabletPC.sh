f2f5f41f1293bc757baf792dd04444c45f0bf0cf
Fumito NISHIKAWA <fmt.floatlamp+git@gmail.com>
2019-05-16 20:52:44 +0900

    Replace some variant to array

--- RotateTabletPC.sh
+++ RotateTabletPC.sh
@@ -3,19 +3,26 @@
 #シンプルに通常の画面をPCモード、縦画面をタブレットモードとして切り替える
 
 #対象のデバイスのIDは　xinput list で確認
-touch_display_multi=11
-touch_display_stylus=18
-trackpad=13
 
-rotation=$(xrandr -q --verbose | grep eDP-1 | egrep -o "(normal|left)" | head -1)
+input_rotate=(11 18)
+input_disable=(13)
+output="eDP-1"
+
+rotation=$(xrandr -q --verbose | grep $output | egrep -o "(normal|left)" | head -1)
 if [ "$rotation" = "normal" ] ; then
-    xrandr --output eDP-1 --rotate left
-    xinput set-prop $touch_display_multi 'Coordinate Transformation Matrix' 0 -1 1 1 0 0 0 0 1
-    xinput set-prop $touch_display_stylus 'Coordinate Transformation Matrix' 0 -1 1 1 0 0 0 0 1
-    xinput set-prop $trackpad 'Device Enabled' 0
+    xrandr --output $output --rotate left
+    for var in ${input_rotate[@]};do
+      xinput set-prop $var 'Coordinate Transformation Matrix' 0 -1 1 1 0 0 0 0 1
+    done
+    for var in ${input_disable[@]};do
+      xinput set-prop $var 'Device Enabled' 0
+    done
 else
-    xrandr --output eDP-1 --rotate normal
-    xinput set-prop $touch_display_multi 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
-    xinput set-prop $touch_display_stylus 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
-    xinput set-prop $trackpad 'Device Enabled' 1
+    xrandr --output $output --rotate normal
+    for var in ${input_rotate[@]};do
+      xinput set-prop $var 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
+    done
+    for var in ${input_disable[@]};do
+      xinput set-prop $var 'Device Enabled' 1
+    done
 fi
